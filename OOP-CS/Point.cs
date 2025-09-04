c9616eceb7468ae74f1fd1f9a3bea53cec99c3cf
Ada <Ada@PROBOOK>
2025-09-04 19:42:01 +0700

    Add project files.

---
+++ OOP-CS/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Point
+    {
+        //double x, y;
+
+        //Properties:
+        //public double X
+        //{
+        //    get { return x; }
+        //    set { x = value; }
+        //}
+        //public double Y
+        //{
+        //    get { return y; }
+        //    set { y = value; }
+        //}
+        //public double GetX()
+        //{
+        //    return x;
+        //}
+        //public double GetY()
+        //{
+        //    return y;
+        //}
+        //public void SetX(double x)
+        //{
+        //    this.x = x;
+        //}
+        //public void SetY(double y)
+        //{
+        //    this.y = y;
+        //}
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        //methods
+        public void Print()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+        }
+
+        public double Distance(Point other)
+        {
+            return Math.Sqrt((this.X - other.X) * (this.X - other.X) + (this.Y - other.Y) * (this.Y - other.Y));
+        }
+    }
+}
---
+++ OOP-CS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point();
+            //point.SetX(2);
+            //point.SetY(3);
+            // point1.Print();
+
+            point1.X = 7;
+            point1.Y = 8;
+            Console.WriteLine($"p1: X = {point1.X}, Y = {point1.Y}");
+
+            Point point2 = new Point();
+
+            point2.X = 10;
+            point2.Y = 12;
+            Console.WriteLine($"p2: X = {point2.X}, Y = {point2.Y}");
+
+            Console.WriteLine($"The distanse between points is equal {point1.Distance(point2)}");
+        }
+    }
+}
